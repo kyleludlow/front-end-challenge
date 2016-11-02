c1d3814c990bd1245dd8c0ecf0e189f131ba8e8d
Kyle <kyleludlow@gmail.com>
2016-11-02 15:32:53 -0700

    replaces for loops with map methods for readability, adds an ES6 solution just in case, and tweaks mobile display.

--- script.js
+++ script.js
@@ -16,11 +16,12 @@
   };
 
   ProductContainer.prototype.pushProductsToArray = function(productResponse) {
-    for (var i = 0; i < productResponse.sales.length; i++){
-      this.products.push(new Product(productResponse.sales[i], i));
-    }
+    var self = this;
+    productResponse.sales.map(function(product, index) {
+      return self.products.push(new Product(product, index));
+    });
 
-    return this.getProductTemplate();
+    return self.getProductTemplate();
   }
 
   ProductContainer.prototype.getProductTemplate = function() {
@@ -33,21 +34,21 @@
 
   ProductContainer.prototype.updateProductHtml = function(template) {
 
-    for (var i = 0; i < this.products.length; i++) {
-      this.products[i].updateHtml(template);
-    }
+    this.products.map(function(product) {
+      return product.updateHtml(template)
+    });
 
     return this.updateDom();
   };
 
   ProductContainer.prototype.updateDom = function() {
+    var thisHtml = '';
 
-    thishtml = '';
-    for (var i = 0; i < this.products.length; i++){
-      thishtml += this.products[i].htmlview;
-    }
+    this.products.map(function(product) {
+      return thisHtml += product.htmlview
+    });
 
-    $("#content").append(thishtml)
+    $("#content").append(thisHtml)
     this.addRemoveProductEvent();
   };
 
@@ -56,7 +57,6 @@
   }
 
   ProductContainer.prototype.removeProduct = function(e) {
-    console.log(e);
     e.preventDefault();
     var productToRemove = $(this.closest('.product-container'));
     return productToRemove.hide('slow');
@@ -91,3 +91,99 @@
   })();
 
 })(window.jQuery);
+
+
+
+/****************************
+  ES6 (if it were usable)
+****************************/
+
+/*
+
+($ => {
+
+
+ // PRODUCT CONTAINER
+
+  function ProductContainer() {
+    this.products = [];
+  }
+
+  ProductContainer.prototype.getProducts = url => {
+    let self = this;
+    return $.getJSON(url)
+      .then(response => self.pushProductsToArray(response));
+  };
+
+  ProductContainer.prototype.pushProductsToArray = productResponse => {
+
+    productResponse.sales.map((product, index) => {
+      return this.products.push(new Product(product, index));
+    });
+
+    return this.getProductTemplate();
+  }
+
+  ProductContainer.prototype.getProductTemplate = () => {
+    let self = this;
+    return $.get('product-template.html')
+      .then(template => self.updateProductHtml(template));
+  };
+
+  ProductContainer.prototype.updateProductHtml = template => {
+
+    this.products.map(product => product.updateHtml(template));
+
+    return this.updateDom();
+  };
+
+  ProductContainer.prototype.updateDom = () => {
+    let thisHtml = '';
+
+    this.products.map(product => thisHtml += product.htmlview);
+
+    $("#content").append(thisHtml)
+    this.addRemoveProductEvent();
+  };
+
+  ProductContainer.prototype.addRemoveProductEvent = () => {
+    return $('#content').on('click', '.remove-product-x', this.removeProduct);
+  }
+
+  ProductContainer.prototype.removeProduct = e => {
+    e.preventDefault();
+    let productToRemove = $(this.closest('.product-container'));
+    return productToRemove.hide('slow');
+  }
+
+ // PRODUCT
+
+  function Product(product, i) {
+    this.photo        = product.photos.medium_half
+    this.title        = product.name
+    this.tagline      = product.tagline
+    this.url          = product.url
+    this.description  = product.description
+    this.htmlview     = ""
+    this.index        = i
+  }
+
+  Product.prototype.updateHtml = template => {
+    return this.htmlview = template.replace('{image}', this.photo)
+                                   .replace('{title}', this.title)
+                                   .replace('{tagline}', this.tagline)
+                                   .replace('{url}', this.url)
+                                   .replace('{description}', this.description)
+  };
+
+
+// INITIATE APP
+
+  (function init() {
+    var productContainer = new ProductContainer();
+    productContainer.getProducts('data.json');
+  })();
+
+})(window.jQuery);
+
+*/
